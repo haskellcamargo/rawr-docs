408460e1c603ab704a3ae8a974b9b7ade66591de
Marcelo Camargo <marcelocamargo@linuxmail.org>
2015-02-21 06:27:45 -0200

    Update example.php

--- example.php
+++ example.php
@@ -1,9 +1,10 @@
-$age = Int (18);
-$ableToDrive = Lambda (
-  /** :: Int -> Bool */
-  function (Int $age) {
-    return $age -> greaterOrEq (18);
-  }
-);
-
-$ableToDrive($age) -> inspect (); // Object (bool);
+<?php
+  $age = Int (18);
+  $ableToDrive = Lambda (
+    /** :: Int -> Bool */
+    function (Int $age) {
+      return $age -> greaterOrEq (18);
+    }
+  );
+  
+  $ableToDrive($age) -> inspect (); // Object (bool);
