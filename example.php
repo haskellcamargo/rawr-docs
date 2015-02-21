eb08bc357791b0f5baa2ad43b451b8d7e0006387
Marcelo Camargo <marcelocamargo@linuxmail.org>
2015-02-21 06:19:22 -0200

    Create example.php

---
+++ example.php
@@ -0,0 +1,9 @@
+$age = Int (18);
+$ableToDrive = Lambda (
+  /** :: Int -> Bool */
+  function (Int $age) {
+    return $age -> greaterOrEq (18);
+  }
+);
+
+$ableToDrive($age) -> inspect (); // Object (bool);
